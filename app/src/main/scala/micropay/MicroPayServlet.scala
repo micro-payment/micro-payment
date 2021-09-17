22c60ec79156bedc84f2dd5db699ec2f06282f92
Martin Schoeberl <martin@jopdesign.com>
2021-09-17 16:32:09 +0200

    app: add a form

--- app/src/main/scala/micropay/MicroPayServlet.scala
+++ app/src/main/scala/micropay/MicroPayServlet.scala
@@ -2,10 +2,45 @@ package micropay
 
 import org.scalatra._
 
+import scala.xml._
+
 class MicroPayServlet extends ScalatraServlet {
+
+  private def displayPage(title:String, content:Seq[Node]): Elem = {
+    <html lang="en">
+      <head>
+        <title>{ title }</title>
+        <meta charset="utf-8" />
+      </head>
+
+      <body>
+        {content}
+      </body>
+
+    </html>
+  }
+
 
   get("/") {
     views.html.hello()
+    "Hello Micropayment\nFor more info see: https://micro-payment.github.io/micro-payment/"
+    displayPage("Scalatra: Form Post Example",
+      <pre> Register as new user</pre>
+      <form action={url("/post")} method='POST'>
+        email: <input name="email" type='text'/>
+        <p></p>
+        password: <input name="pwd" type='password'/>
+        <p></p>
+        <input type='submit'/>
+      </form>
+        <pre>Read our mission statement at </pre> <a href={fullUrl("https://micro-payment.github.io/micro-payment/")}>MicroPayment</a>
+    )
+  }
+
+  post("/post") {
+    displayPage("Scalatra: Form Post Example",
+      <p>Welcome: {params("email")}</p>
+    )
   }
 
 }
